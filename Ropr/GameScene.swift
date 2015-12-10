e85be9997dbf8a02cc5b1adc99b008ce17effca7
Brendon Duan Ho <Him@woo-jin-hos-imac.local>
2015-12-09 22:01:00 -0500

    ayy

--- Ropr/GameScene.swift
+++ Ropr/GameScene.swift
@@ -9,20 +9,47 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var man = SKSpriteNode()
+    
     override func didMoveToView(view: SKView) {
-   
+        //Physics, I suck At It
+        self.physicsWorld.gravity = CGVectorMake(0.0,-5.0)
         
         //Water!
-        let lolTex = SKSpriteNode.init(imageNamed: "Water.png")
-        lolTex.setScale(2.0)
-        lolTex.position = CGPointMake(CGRectGetMidX(self.frame),140)
-        self.addChild(lolTex)
+        let lolTex = SKTexture(imageNamed: "Waterbetter.png")
+        let lolTexSprite = SKSpriteNode(texture: lolTex)
+        lolTexSprite.setScale(2.0)
+        lolTexSprite.position = CGPointMake(self.size.width/2, 140)
+        
+        let water = SKNode()
+        water.position = CGPointMake(0,lolTex.size().height)
+        water.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, lolTex.size().height * 2.0))
+        water.physicsBody?.dynamic = false
+        self.addChild(water)
+        //Character!
+        let manTex = SKTexture(imageNamed: "Char.png")
+        
+        man = SKSpriteNode(texture: manTex)
+        man.setScale(0.5)
+        man.position = CGPoint(x: self.frame.size.width * 0.35, y: self.frame.size.height * 0.6)
+        man.physicsBody = SKPhysicsBody(circleOfRadius: man.size.height/2.0)
+        man.physicsBody!.dynamic = true
+        man.physicsBody?.allowsRotation = false
+        self.addChild(man)
+        
+        //Ropes!
+        let ropeTex = SKSpriteNode.init(imageNamed: "")
+        ropeTex.setScale(2.0)
+        
         
        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
+        for touch: AnyObject in touches {
+        }
 
         }
     }
